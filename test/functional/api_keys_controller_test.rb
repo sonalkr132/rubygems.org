06fbed4dd034bc9e452f0bbfd25bed4fdc20fa46
Aditya Prakash <aditya.prakash132@gmail.com>
2019-05-10 00:52:57 +0530

    Add tests for api keys

---
+++ test/functional/api_keys_controller_test.rb
@@ -0,0 +1,134 @@
+require "test_helper"
+
+class ApiKeysControllerTest < ActionController::TestCase
+  context "when not logged in" do
+    context "on GET to index" do
+      setup { get :index }
+
+      should redirect_to("the homepage") { root_path }
+    end
+
+    context "on GET to new" do
+      setup { get :new }
+
+      should redirect_to("the homepage") { root_path }
+    end
+
+    context "on POST to create" do
+      setup { post :create, params: { api_key: { name: "test", add_owner: true } } }
+
+      should redirect_to("the homepage") { root_path }
+    end
+
+    context "on DELETE to destroy" do
+      setup { post :create, params: { id: 1 } }
+
+      should redirect_to("the homepage") { root_path }
+    end
+  end
+
+  context "when logged in" do
+    setup do
+      @user = create(:user)
+      sign_in_as(@user)
+    end
+
+    context "on GET to index" do
+      context "no api key exists" do
+        setup do
+          get :index
+        end
+
+        should redirect_to("the new api key page") { new_profile_api_key_path }
+      end
+
+      context "api key exists" do
+        setup do
+          @api_key = create(:api_key, user: @user)
+          get :index
+        end
+
+        should respond_with :success
+
+        should "render api key of user" do
+          assert page.has_content? @api_key.name
+        end
+      end
+    end
+
+    context "on GET to new" do
+      setup { get :new }
+
+      should respond_with :success
+
+      should "render api key of user" do
+        assert page.has_content? "New Api key"
+      end
+    end
+
+    context "on POST to create" do
+      context "with successful save" do
+        setup { post :create, params: { api_key: { name: "test", add_owner: true } } }
+
+        should redirect_to("the key index page") { profile_api_keys_path }
+        should "create new key for user" do
+          api_key = @user.api_keys.last
+
+          assert_equal "test", api_key.name
+          assert api_key.add_owner?
+        end
+      end
+
+      context "with unsuccessful save" do
+        setup { post :create, params: { api_key: { add_owner: true } } }
+
+        should "show error to user" do
+          assert page.has_content? "Name can't be blank"
+        end
+
+        should "not create new key for user" do
+          assert @user.reload.api_keys.empty?
+        end
+      end
+    end
+
+    context "on DELETE to destroy" do
+      context "user is owner of key" do
+        setup { @api_key = create(:api_key, user: @user) }
+
+        context "with successful destroy" do
+          setup { delete :destroy, params: { id: @api_key.id } }
+
+          should redirect_to("the index api key page") { profile_api_keys_path }
+          should "delete api key of user" do
+            assert @user.api_keys.empty?
+          end
+        end
+
+        context "with unsuccessful destroy" do
+          setup do
+            ApiKey.any_instance.stubs(:destroy).returns(false)
+            delete :destroy, params: { id: @api_key.id }
+          end
+
+          should redirect_to("the index api key page") { profile_api_keys_path }
+          should "not delete api key of user" do
+            refute @user.api_keys.empty?
+          end
+        end
+      end
+
+      context "user is not owner of key" do
+        setup do
+          @api_key = create(:api_key)
+          delete :destroy, params: { id: @api_key.id }
+        end
+
+        should respond_with :not_found
+        should "not delete the api key" do
+          assert ApiKey.find(@api_key.id)
+        end
+      end
+    end
+  end
+end
---
+++ test/integration/api_keys_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class ApiKeysTest < SystemTest
+  setup do
+    @user = create(:user)
+
+    visit sign_in_path
+    fill_in "Email or Username", with: @user.email
+    fill_in "Password", with: @user.password
+    click_button "Sign in"
+  end
+
+  test "creating new api key" do
+    visit profile_api_keys_path
+
+    fill_in "api_key[name]", with: "test"
+    check "api_key[index_rubygems]"
+    click_button "Create"
+
+    assert page.has_content? "6 months"
+  end
+
+  test "deleting api key" do
+    create(:api_key, user: @user)
+
+    visit profile_api_keys_path
+    click_link "Delete"
+
+    assert page.has_content? "New Api key"
+  end
+end
