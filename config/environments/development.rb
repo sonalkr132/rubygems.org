d29e2128dc4bf23b68c78892b99ab52ac8efedee
Aditya Prakash <aditya.prakash132@gmail.com>
2019-06-01 14:24:33 +0530

    Run rails app:update
    Chose not to overwrite config/locales/en.yml, config/initializers/content_security_policy.rb
    and config/routes.rb. The files get overridden to prestine rails app config,
    with no meaningful changes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,10 +16,9 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :mem_cache_store,
-                         'localhost:11211',
-                         { compress: true, compression_min_size: 524_288 }
+    config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
@@ -33,10 +32,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { host: Gemcutter::HOST,
-                                               protocol: Gemcutter::PROTOCOL }
-  config.action_mailer.preview_path = Rails.root.join("lib")
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -55,10 +50,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-
-  config.middleware.use Hostess
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
